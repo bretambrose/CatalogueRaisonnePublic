6fd75511d4e01043aaec6a40e99e2d3b1369367d
BretAmbrose <bretambrose@gmail.com>
2011-10-30 09:55:25 -0700

    GPLified

--- CatalogueRaisonne/CRClientShared/Source/Game/ClientUIMatchStuff.cs
+++ CatalogueRaisonne/CRClientShared/Source/Game/ClientUIMatchStuff.cs
@@ -1,4 +1,25 @@
-﻿// A temp file archiving rewritten functionality that will eventually be needed by the UI thread in its separate game state managenment logic
+﻿/*
+
+	ClientUIMatchStuff.cs
+
+	(c) Copyright 2010-2011, Bret Ambrose (mailto:bretambrose@gmail.com).
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+*/
+
+// A temp file archiving rewritten functionality that will eventually be needed by the UI thread in its separate game state managenment logic
 using System;
 
 using CRShared;
